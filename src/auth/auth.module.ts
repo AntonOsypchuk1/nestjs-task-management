259c484f20aa9f8dfd3638b073667bd55bf7d33a
AntonOsypchuk1 <osypchuk.ua@gmail.com>
2024-07-24 23:16:05 +0300

    feature: Sign in with jwt token and validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user.entity';
 import { UsersRepository } from './users.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([UsersRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
